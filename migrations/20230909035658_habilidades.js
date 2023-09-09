1480f6203188999b1a2dede20bd0eecaacdbfefb
LAUVM10 <ing.lauvm@gmail.com>
2023-09-09 00:06:12 -0500

    tabla de habilidades

--- migrations/20230909035658_habilidades.js
+++ migrations/20230909035658_habilidades.js
@@ -3,7 +3,15 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.hasTable('habilidades').then((exists) => {
+        if (!exists) {
+            return knex.schema.createTable("habilidades", function (table) {
+                table.increments("id_habilidad").primary();
+                table.string("descripción").notNullable();
+            });
+        }
+  });
+
 };
 
 /**
