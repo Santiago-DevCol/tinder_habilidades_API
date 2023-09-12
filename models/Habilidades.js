13c0ad3e084f9ef4f25ec5abb76302cc39755a72
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2023-09-11 22:57:45 -0500

    fix: correccion metodo para obtener todas las habilidades

--- models/Habilidades.js
+++ models/Habilidades.js
@@ -3,25 +3,25 @@ const connection = require("../knexfile")['development'];
 const database = require('knex')(connection);
 
 
- const createHabilidad = (habil) => {
-    return database('habilidades')
-    .insert(habil).returning('id');
- };
+const createHabilidad = (habil) => {
+   return database('habilidades')
+   .insert(habil).returning('id_habilidad');
+};
 
- const getHabilidad = (habil) => {
-    console.log('habilidades',habil)
-    return database('habilidades')
-    .where({idHabilidad:habil})
-    .then((respuesta) => {
-       return respuesta.message[0];
-    });
- };
+const getHabilidad = (habil) => {
+   console.log('habilidades',habil)
+   return database('habilidades')
+   .where({idHabilidad:habil})
+   .then((respuesta) => {
+      return respuesta.message[0];
+   });
+};
 
- const getAllHabilidades = (habil) => {
-    return database.select('*').from('habilidades');
- };
+const getAllHabilidades = () => {
+   return database.select('*').from('habilidades');
+};
 module.exports = {
-    createHabilidad,
-    getHabilidad,
-    getAllHabilidades
+   createHabilidad,
+   getHabilidad,
+   getAllHabilidades
 };
