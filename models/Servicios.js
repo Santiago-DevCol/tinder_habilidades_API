d5a3516e06c4be0d2564e3ffb9406eb7e9189288
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2023-09-16 01:23:44 -0500

    fix: ajuste a las rutas de servicios para crearlos

--- models/Servicios.js
+++ models/Servicios.js
@@ -3,14 +3,14 @@ const connection = require("../knexfile")['development'];
 const database = require('knex')(connection);
 
  const createServicios = (Servicios) => {
-    return database('Servicios')
+    return database('servicios')
     .insert(Servicios);
  };
  const getServicios = (Servicios) => {
    let Serviciosid = Servicios;
    // console.log('Servicios',Servicios)
-   return database('Servicios')
-   .where({id:Serviciosid})
+   return database('servicios')
+   .where({id_servicio:Serviciosid})
    // .then((respuesta) => {
    //    return respuesta.message[Servicios];
    // });
@@ -18,8 +18,8 @@ const database = require('knex')(connection);
 /*Or const getOneClient = (id) => {
    return database.select('*').from('cliente').where('dni',id);
 } */
-const getAllServicios = (Servicios) => {
-   return database.select('*').from('Servicios');
+const getAllServicios = () => {
+   return database.select('*').from('servicios');
 };
 
 
