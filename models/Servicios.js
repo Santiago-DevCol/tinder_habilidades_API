dc1031d3b513f9da07a91fe8d989f90cfd036a04
Wilson Rocha <wijaro88@gmail.com>
2023-09-16 23:52:21 -0500

    upload querys to services andd habilities

--- models/Servicios.js
+++ models/Servicios.js
@@ -2,23 +2,52 @@ const connection = require("../knexfile")['development'];
 
 const database = require('knex')(connection);
 
- const createServicios = (Servicios) => {
-    return database('servicios')
-    .insert(Servicios);
- };
- const getServicios = (Servicios) => {
-   let Serviciosid = Servicios;
+const createServicios = (Servicios) => {
    return database('servicios')
-   .where({id_servicio:Serviciosid})
+      .insert(Servicios);
 };
+const getServicios = (Servicios) => {
+   // let Serviciosid = Servicios;
+   // console.log('Servicios',Servicios)
 
+
+
+   return database.select('servicios.id_servicio','personas.id_persona', 'personas.nombre_persona', 'personas.precio_servicio', 'servicios.descripción',
+      'empresa.nombre_empresa', 'servicios.status')
+      .from('personas')
+      .join('servicios', function () {
+         this
+            .on('servicios.fk_persona_id', '=', 'personas.id_persona')
+      })
+      .join('empresa', function () {
+         this
+            .on('empresa.id', '=', 'servicios.fk_empresa_id')
+      })
+      .where({ id_servicio: Servicios});
+   // .then((respuesta) => {
+   //    return respuesta.message[Servicios];,
+   // });
+};
+/*Or const getOneClient = (id) => {
+   return database.select('*').from('cliente').where('dni',id);
+} */
 const getAllServicios = () => {
-   return database.select('*').from('servicios');
+   return database.select('servicios.id_servicio','personas.id_persona', 'personas.nombre_persona', 'personas.precio_servicio', 'servicios.descripción',
+      'empresa.nombre_empresa', 'servicios.status')
+      .from('personas')
+      .join('servicios', function () {
+         this
+            .on('servicios.fk_persona_id', '=', 'personas.id_persona')
+      })
+      .join('empresa', function () {
+         this
+            .on('empresa.id', '=', 'servicios.fk_empresa_id')
+      });
 };
 
 
- module.exports = {
-    createServicios,
-    getServicios,
-    getAllServicios
- };
+module.exports = {
+   createServicios,
+   getServicios,
+   getAllServicios
+};
