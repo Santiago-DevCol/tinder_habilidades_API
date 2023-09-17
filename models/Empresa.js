6b63b874b04d55bf36175daa166399be0b7f8eb4
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2023-09-16 22:41:58 -0500

    feat: encriptar pw empresas

--- models/Empresa.js
+++ models/Empresa.js
@@ -1,11 +1,22 @@
 const connection = require("../knexfile")['development'];
+const bcrypt = require("bcrypt");
 
 const database = require('knex')(connection);
 
 
- const createEmpresa = (empresa) => {
-    return database('empresa')
-    .insert(empresa);
+ const createEmpresa = async (empresa) => {
+
+   try{
+      let hashPw = empresa.password;
+
+      empresa.password = await encryptPassword(hashPw);
+      //console.log(empresa);
+
+      return database('empresa')
+      .insert(empresa);
+   }catch (error){  
+
+   }
  };
  const getEmpresa = (empresa) => {
    let empresaid = empresa;
@@ -23,6 +34,16 @@ const getAllEmpresas = (empresa) => {
    return database.select('*').from('empresa');
 };
 
+async function encryptPassword (pw){
+   const rounds = 10;
+   try{
+      const hashPassword = await bcrypt.hash(pw, rounds);
+      //console.log(hashPassword)
+      return hashPassword;
+   }catch (error){
+      console.log(`parece que hay un error: ${error}`);
+   }
+}
 
  module.exports = {
     createEmpresa,
