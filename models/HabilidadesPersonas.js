8ea8b6052bafcc3da121ea20cfb5483fb2e69706
Wilson Rocha <wijaro88@gmail.com>
2023-09-18 16:46:38 -0500

    aun no me funciona esta consulta :,(

--- models/HabilidadesPersonas.js
+++ models/HabilidadesPersonas.js
@@ -5,29 +5,29 @@ const database = require('knex')(connection);
 
 const createHabilidadPersona = (habil) => {
    return database('habilidad_personas')
-   .insert(habil).returning('id_persona_habilidad');
+      .insert(habil).returning('id_persona_habilidad');
 };
 
-const getHabilidadPersonas = (habil) => {
-   // let habilidadPersonaid = atob(habil);
-   ///console.log('habilidades',habilidadPersonaid)
-   return database.select ('personas.id_persona','personas.nombre_persona','habilidades.id_habilidad','habilidades.descripción')
-   .from ('habilidad_personas')
-   .join ('personas', function (){
-      this  .on ('personas.id_persona', '=', 'habilidad_personas.fk_persona_id')
-   })
-   .join ('habilidades', function(){this .on ('habilidades.id_habilidad' ,'=' ,'habilidad_personas.fk_habilidad_id')
-})
+const getHabilidadPersonas = (id_persona) => {
 
-
-   .where('id_persona_habilidad',habil)
+   return database
+      .select(
+         'personas.id_persona',
+         'personas.nombre_persona',
+         'habilidades.id_habilidad',
+         'habilidades.descripción'
+      )
+      .from('habilidad_personas')
+      .innerJoin('personas','habilidad_personas.fk_persona_id', '=', 'personas.id_persona')
+      .innerJoin('habilidades','habilidad_personas.fk_habilidad_id' , '=', 'habilidades.id_habilidad')
+      .where('personas.id_persona', id_persona);
 };
 
 const getAllHabilidadesPersonas = () => {
    return database.select('*').from('habilidad_personas');
 };
 module.exports = {
-    createHabilidadPersona,
-    getHabilidadPersonas,
-    getAllHabilidadesPersonas
+   createHabilidadPersona,
+   getHabilidadPersonas,
+   getAllHabilidadesPersonas
 };
